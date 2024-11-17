271951f31f76b38beddf8e4648b4d102f9edfc3f
Abhishek kumar <89406337+AbhishekKr2001@users.noreply.github.com>
2024-11-17 22:51:01 +0530

    Add files via upload

---
+++ Mini Project/my-fin-bank/src/bank.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './bank.css';
+export default function Bank() {
+    return (
+        <>
+            <div className="div1">
+                <div className="one"><h1>MY FIN BANK</h1>
+                    <h5>Paisa he Paisa Hoga</h5></div>
+                <div className="second"><h4>Created By Abhishek Kumar</h4></div>
+            </div>
+        </>
+    )
+}
